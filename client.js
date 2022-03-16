0e91414fe9d691029f2ea9eb04d7091667173b77
Trishnarjun Kunamalla <trishnarjun.k@gmail.com>
2022-03-16 17:22:00 +0000

    client added connect success msg

--- client.js
+++ client.js
@@ -6,9 +6,13 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    console.log('Name: Arjun Successfully connected to game server');
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
 
   return conn;
 };
