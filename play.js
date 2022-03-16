734773ad064354ad1444b312957809315391c45e
Trishnarjun Kunamalla <trishnarjun.k@gmail.com>
2022-03-16 18:12:21 +0000

    input added

--- play.js
+++ play.js
@@ -1,11 +1,26 @@
-
+const net = require("net");
 const connect = require("./client");
 
-// establishes a connection with the game server
-
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function() {
+  process.stdin.on('data', (key) => {
+    process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
 
 
 
 
 console.log("Connecting ...");
 connect();
+setupInput();
